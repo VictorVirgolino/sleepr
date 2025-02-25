fade067e679b2d6dc5c426d73bd09e8b4dfd457c
VictorVirgolino <victor@DESKTOP-9DIF3SS>
2025-02-25 15:31:29 -0300

    jwt authentication ok

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -2,6 +2,7 @@ import {
   Injectable,
   NotFoundException,
   UnauthorizedException,
+  UnprocessableEntityException,
 } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
@@ -13,11 +14,21 @@ export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   async create(createUserDto: CreateUserDTO) {
+    await this.validateCreateUserDTO(createUserDto);
     return this.usersRepository.create({
       ...createUserDto,
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
       password: await bcrypt.hash(createUserDto.password, 10),
     });
+  }
+
+  private async validateCreateUserDTO(createUserDto: CreateUserDTO){
+    try {
+      await this.usersRepository.findOne({email: createUserDto.email})
+    } catch (error) {
+      return;
+    }
+    throw new UnprocessableEntityException("Email already exists.")
   }
 
   async verifyUser(email: string, password: string) {
